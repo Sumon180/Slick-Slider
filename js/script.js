11bd8699aea846fec72f0f5ba347d48ba6643af9
sumon-hossain <sumonengr.96@gmail.com>
2023-03-29 09:53:42 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(".center").slick({
+  centerMode: true,
+  centerPadding: "60px",
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 1080,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 2,
+      },
+    },
+  ],
+});
